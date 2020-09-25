ead95636c2d3bb069245e5ea3f8724e52a585450
Candy Capilla <candycapilla@MacBook-Pro-2.local>
2020-09-25 15:52:09 -0400

    Sep25_3:52

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,66 +11,34 @@ months = []
 change = []
 profit_loss = []
 
+print(f"Financial Analysis")
+print(f"--------------------------")
+
 with open(budgetcsvpath) as csvfile:
    csvreader = csv.reader(csvfile, delimiter=",")
    next(csvreader, None)   
    for row in csvreader:
        total_months += 1
-   print(total_months) 
+       net_total += int(row[1])
+       profit_loss.append(int(row[1]))
+       months.append(row[0])
 
-with open(budgetcsvpath) as csvfile:
-   csvreader = csv.reader(csvfile, delimiter=",")
-   next(csvreader, None)
-   for row in csvreader:
-        net_total += float(row[1])
-    
-   print(net_total)
+   print(f"Total Months: {total_months}") 
 
-with open(budgetcsvpath) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
-    next(csvreader,None)
+   print(f"Total: $ {net_total}")
 
-    for row in csvreader:
-        profit_loss.append(int(row[1]))
-        months.append(row[0])
-
-    for i in range(1,len(profit_loss)):
+   for i in range(1,len(profit_loss)):
         change.append((int(profit_loss[i]) - int(profit_loss[i-1])))
 
-    average_change = sum(change)/ len(change)
+   average_change = sum(change)/ len(change)
 
-    print(average_change)
+   print(f"Average Change: {round(average_change,2)}")
 
-# def financial_analysis(budget_data):
-#     month = str(budget_data[0])
-#     profit_loss = int(budget_data[1])
-#     total_months = len(month)
-#     net_total = []
-#     net_total.append(int(profit_loss) + int(profit_loss + 1))
-#     change = []
-#     change.append(int(profit_loss) - int(profit_loss - 1))
-#     average_change = (sum(change)/ len(change)
-#     greatest_increase = max(average_change)
-#     greatest_decrease = min(average_change)
-#     print(f"Financial Analysis")
-#     print(f"______________________________________________________________________________________")
-#     print(f"Total months:   {str(total_months)}")
-#     print(f"Total:   ${str(net_total)}")
-#     print(f"Average change:   ${str(average_change)}")
-#     print(f"Greatest Increase in Profits:   {str(month[average_change.index(Greatest_increase))+1])}  ${str(greatest_increase)}")
-#     print(f"Greatest Decrease in Profits:  {str(month[average_change.index(Greatest_decrease))+1])}  ${str(greatest_decrease)}"))
-#     # output to a text file
-#    # file = open("output.txt","w")
-#    # file.write("Financial Analysis" + "\n")
-#    # file.write("...................................................................................." + "\n")
-#    # file.write("total months: " + str(total_months) + "\n")
-#    # file.write("Total: " + "$" + str(sum(P)) + "\n")
-#    # file.write("Average change: " + "$" + str(revenue_average) + "\n")
-#    # file.write("Greatest Increase in Profits: " + str(months[revenue_change.index(max(revenue_change))+1]) + " " + "$" + str(greatest_increase) + "\n")
-#    # file.write("Greatest Decrease in Profits: " + str(months[revenue_change.index(min(revenue_change))+1]) + " " + "$" + str(greatest_decrease) + "\n")
-#    # file.close()
-# with open(budgetcsvpath) as csvfile:
-#     csvreader = csv.reader(csvfile, delimiter=",")
-#     next(csvreader, None)
-#     for row in csvreader:
-#         financial_analysis(row)
+
+greatest_increase = max(change)
+greatest_decrease = min(change)
+
+
+print(f"Greatest Increase: {months[change.index(max(change)) + 1]} (${(str(greatest_increase))})")
+print(f"Greatest Decrease: {months[change.index(min(change)) + 1]} (${(str(greatest_decrease))})")
+
