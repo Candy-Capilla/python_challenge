fa0a023bef0934322462133fad502f18c3fd8f43
Candy Capilla <candycapilla@MacBook-Pro-2.local>
2020-09-26 08:11:03 -0400

    Sep26_8:10

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,10 +10,10 @@ net_total = 0
 months = []
 change = []
 profit_loss = []
-
+#Print info
 print(f"Financial Analysis")
 print(f"--------------------------")
-
+#Sum total months and net total, add to months and proft loss list
 with open(budgetcsvpath) as csvfile:
    csvreader = csv.reader(csvfile, delimiter=",")
    next(csvreader, None)   
@@ -26,27 +26,27 @@ with open(budgetcsvpath) as csvfile:
    print(f"Total Months: {total_months}") 
 
    print(f"Total: $ {net_total}")
-
+#create list of change in profit/loss
    for i in range(1,len(profit_loss)):
         change.append((int(profit_loss[i]) - int(profit_loss[i-1])))
-
+#Calculate average change
    average_change = sum(change)/ len(change)
-
+#Print info
    print(f"Average Change: {round(average_change,2)}")
 
-
+#Calculate greatest increase and decrease
 greatest_increase = max(change)
 greatest_decrease = min(change)
 
-
+#Print info
 print(f"Greatest Increase: {months[change.index(max(change)) + 1]} (${(str(greatest_increase))})")
 print(f"Greatest Decrease: {months[change.index(min(change)) + 1]} (${(str(greatest_decrease))})")
 
-# location of open output files
+#location of open output files
 output_file = os.path.join("Analysis", "Financial_Analysis_Summary.txt")
 with open(output_file,"w") as file:
 
-# Write output file
+#Write output file
     file.write("Financial Analysis")
     file.write("\n")
     file.write("----------------------------")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,31 +1,55 @@
-#import moduels
+#import modules
 import os
 import csv
-#set path for file
+#set path file
 dirname = os.path.dirname(__file__)
 electioncsvpath = os.path.join("Resources", "Election_data.csv")
 #declare variables
 candidates = {}
 total_votes = 0
-
+#open csv and skip header
 with open(electioncsvpath) as csvfile:
    csvreader = csv.reader(csvfile, delimiter=",")
-   next(csvreader, None)   
+   next(csvreader, None)
+#sum total votes and find candidates that received votes
    for row in csvreader:
        total_votes = total_votes + 1
        if row[2] not in candidates:
            candidates[row[2]] = 1
        else:
            candidates[row[2]] = candidates [row[2]] + 1
-print("Election Results")  
-print("-----------------------------")        
-print(f"Total Votes: {total_votes}")
-print("-----------------------------")
-max_votes = 0
-winner = ""
-for name,votes in candidates.items():
-    print(f"{name}: {100*votes/total_votes:.1f}%: {votes}:")
-    if votes> max_votes:
-        winner = name
-        max_votes = votes 
-print(f"Winner: {winner}")
+#create function to total candidate votes, declare winner, print results, and write to file
+def candidate_votes(Election_data):
+    output_file = os.path.join("Analysis", "Election_Results.txt")
+    max_votes = 0
+    winner = ""
+    with open(output_file,"w") as file:
+        file.write("Election Results")
+        file.write("\n")
+        file.write("----------------------------")
+        file.write("\n")
+        file.write(f"Total Votes: {total_votes}")
+        file.write("\n")
+        file.write("-----------------------------")
+        file.write("\n")
+        print("Election Results")  
+        print("-----------------------------")        
+        print(f"Total Votes: {total_votes}")
+        print("-----------------------------")
+        for name,votes in candidates.items():
+            file.write(f"{name}: {100*votes/total_votes:.3f}% ({votes})")
+            file.write("\n")
+            print(f"{name}: {100*votes/total_votes:.3f}% ({votes})")
+            if votes > max_votes:
+                winner = name
+                max_votes = votes
+        file.write("-----------------------------")
+        file.write("\n")
+        file.write(f"Winner: {winner}")
+        file.write("\n")
+        file.write("-----------------------------")
+        print("-----------------------------")
+        print(f"Winner: {winner}")
+        print("-----------------------------")
+#run function
+candidate_votes(row)
